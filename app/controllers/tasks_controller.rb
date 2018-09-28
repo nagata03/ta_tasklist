6137e074dc5ae862713e432be95a011042200943
s.nagata <nagata_sho@kakaku.com>
2018-09-28 03:25:25 +0000

    課題：ログイン認証と一対多

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,26 +1,26 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :require_user_logged_in, only: [:new, :create, :edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update, :destroy]
   
   def index
     @tasks = Task.all
   end
-  
-  def show
-  end
-  
+
   def new
     @task = Task.new
   end
   
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
     
     if @task.save
-      flash[:success] = 'Task は正常に登録できました'
-      redirect_to @task
+      flash[:success] = 'Task を登録しました'
+      redirect_to root_url
     else
+      @tasks = current_user.tasks.order('created_at DESC').page(params[:page])
       flash.now[:danger] = 'Task を登録できませんでした'
-      render :new
+      render 'toppages/index'
     end
   end
   
@@ -29,19 +29,18 @@ class TasksController < ApplicationController
   
   def update
     if @task.update(task_params)
-      flash[:success] = 'Task は正常に更新されました'
+      flash[:success] = 'Task を更新しました'
       redirect_to @task
     else
-      flash.now[:danger] = 'Task は更新されませんでした'
+      flash.now[:danger] = 'Task を更新できませんでした'
       render :edit
     end
   end
   
   def destroy
     @task.destroy
-    
-    flash[:success] = 'Task は正常に削除されました'
-    redirect_to tasks_url
+    flash[:success] = 'Task を削除しました'
+    redirect_back(fallback_location: root_path)
   end
   
   private
@@ -53,4 +52,12 @@ class TasksController < ApplicationController
   def task_params
     params.require(:task).permit(:content, :status)
   end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+  end
+  
 end
